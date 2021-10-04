3151a2c25c599b4e9a8a2f2c14a182554f430a02
Linus13000 <lfeldmann40@googlemail.com>
2021-10-04 19:43:40 +0200

    Revert "Updated to 0.9"
    This reverts commit 8dd1cea71e072740e582a4d7fa78b3c1f8ca6130.

--- functions.py
+++ functions.py
@@ -3,13 +3,8 @@ import asyncio
 import random
 
 def help():
-    embed=discord.Embed(title="Hilfe zum Bot: root", description="Dies ist eine Hilfestellung", color=0x9d1092)
-    embed.add_field(name=".help", value="Zeigt diese Hilfe an", inline=False)
-    embed.add_field(name=".repeat", value="Wiederholt das nach dem Befehl gesagte", inline=False)
-    embed.add_field(name=".autorepeat on / off ", value="Wiederholt alle Textnachrichten außer Befehle", inline=False)
-    embed.add_field(name=".randint x y", value="Zufallszahl von x bis y", inline=False)
-    embed.add_field(name=".joke", value="Haut einen Witz raus", inline=False)
-    return(embed)
+    helplist = open('help.txt', 'r')
+    return(helplist.read())
 
 def repeat(message):
     rest = message.content
@@ -17,40 +12,12 @@ def repeat(message):
         rest = rest.split()
         rest.remove('.repeat')
         rest = " ".join(rest)
-
-    embed=discord.Embed(title='Repeat', description='{} hat "{}" geschrieben.'.format(message.author.nick ,rest), color=0x9d1092)        
-    return(embed)
+    return('{} hat "{}" geschrieben.'.format(message.author.nick ,rest))
 
 def randint(message): 
     rest = message.content
     rest = rest.split()
-    if(len(rest) == 3 and rest[1].isnumeric() and rest[2].isnumeric()):
-        embed=discord.Embed(title="Randint", description='Zufallszahl von {} bis {}:'.format(int(rest[1]), int(rest[2])), color=0x9d1092)
-        embed.add_field(name="Zahl:", value=random.randint(int(rest[1]), int(rest[2])), inline=False)
+    if(rest[1].isnumeric() and rest[2].isnumeric()):
+        return('Zufallszahl von {} bis {}:\r\n**{}**').format(int(rest[1]), int(rest[2]), random.randint(int(rest[1]), int(rest[2])))
     else:
-        embed=discord.Embed(title="Dumm?", description="Es müssen 2 Zahlen aus dem Zahlenbereich der ganzen Zahlen angegeben werden**!**", color=0x9d1092)
-    return(embed)
-
-def joke(message):
-    witze = ['Geht ein Pole an die Kasse.', 
-    '„Wie sehen Sie lesbische Paare?“ – „Full HD.“', 
-    'Beim Sex ist es ja oft so wie mit dem Lieblingsgürtel. Ein Loch weiter vorne war alles noch entspannter.', 
-    'Hab neulich ein Kondom der deutschen Bahn genommen. Ich kam später.',
-    'Wieso kann eine Frau beim Verkehr besser denken? Sie ist mit dem Zentralrechner verbunden.',
-    'Was ist weiß und guckt durchs Schlüsselloch? — Ein Spannbettlaken',
-    'Welches Getränk trinken Firmenchefs? — Leitungswasser',
-    'Was sagt ein Origami-Lehrer zu seinem Schüler? — „Das kannste knicken.“',
-    'Brennholzverleih',
-    'Was findet man beim Kannibalen in der Dusche? — Head and Shoulders',
-    'Was passiert wenn man Cola und Bier gleichzeitig trinkt? — Man colabiert',
-    'Was essen Autos am liebsten? — Parkplätzchen',
-    'Was macht eine Bombe im Bordell? — Puff',
-    'Wie heißt ein Spanier ohne Auto? — Carlos',
-    'Was sitzt auf einem Baum und winkt? — Ein Huhu!',
-    "Was ist niedlich und hüpft qualmend über'n Acker? — Ein Kaminchen!",
-    'Was sagt ein Gen, wenn es ein anderes trifft? — Halogen',
-    'Was ist ein studierter Bauer? — Ein Ackerdemiker',
-    'Was ist gelb und schießt? Eine Banone!']
-    witz = random.choice(witze)
-    embed=discord.Embed(title='Witz', description=witz, color=0x9d1092)
-    return(embed)
+        return('**Dumm?**\r\nEs müssen 2 Zahlen aus dem Zahlenbereich der ganzen Zahlen angegeben werden**!**')
--- main.py
+++ main.py
@@ -3,80 +3,67 @@ import asyncio
 import random
 from functions import *
 
-#Umfragen
+#Umfragen hinzufügen
+#Neuer Mensch auf Server -> Infomessage
+#Randint wenn nur eine Zahl angegeben wurde
+#Weitere Pornoseiten Liste mit random
+
 client = discord.Client()
 
 autorepeat_status = False
 pornoseiten = ['Pornhub', 'Youporn', 'Hamster Porn', 'xHamster', 'xnxx', 'xvideos']
 
-
 @client.event
 async def on_ready():
     print("Eingeloggt als User {}".format(client.user.name))
-    print("Bot Ready!")
     client.loop.create_task(status_task())
-
 
 @client.event
 async def on_message(message):
     if message.author.bot:  
         return
-    if message.content.startswith('!' or 'gs.' or 'b.' or '!!'): 
-        return 
-    elif message.content.startswith('.'):
+    if message.content.startswith('.'):
         if(message.content == '.help'):
-            await message.channel.send(embed=help())
+            await message.channel.send(help())
         elif message.content.startswith('.repeat'):
-            await message.channel.send(embed=repeat(message))
+            await message.channel.send(repeat(message))
+
         elif message.content.startswith('.autorepeat'):
-            await message.channel.send(embed=autorepeat(message))     
+            await message.channel.send(autorepeat(message))
+        
         elif message.content.startswith('.randint'):
-            await message.channel.send(embed=randint(message))
-        elif message.content.startswith('.joke'):
-            await message.channel.send(embed=joke(message))
-#        elif message.content.startswith('.play'):
-#            await play(message)
-#        elif message.content.startswith('.stop'):
-#            await stop()
+            await message.channel.send(randint(message))
         else:
             await message.channel.send("**Dumm?**\r\nSiehe *.help* für mehr Informationen**!**")
+    elif message.content.startswith('!'): 
+        return 
+    elif message.content.startswith('gs.'):
+        return
+    elif message.content.startswith('b.'):
+        return
     elif(autorepeat_status):
-        await message.channel.send(embed=repeat(message))
+        await message.channel.send(repeat(message))
     else:
         return
-
-async def status_task():
-    global pornoseiten
-    while True:
-        await client.change_presence(activity=discord.Game(random.choice(pornoseiten)), status=discord.Status.online)
-        await asyncio.sleep(3)
-def token():
-    token = open('token.txt', 'r')
-    return token.read()
 
 def autorepeat(message):
     global autorepeat_status
     if(message.content == '.autorepeat on' or message.content == '.autorepeat On'):
         autorepeat_status = True
-        embed=discord.Embed(title="Autorepeat", description="Autorepeat ist an", color=0x9d1092)
+        return("**Autorepeat is on**")
     elif(message.content == '.autorepeat off' or message.content == '.autorepeat Off'):
         autorepeat_status = False
-        embed=discord.Embed(title="Autorepeat", description="Autorepeat ist aus", color=0x9d1092)
+        return("**Autorepeat is off**")
     else:
-        embed=discord.Embed(title="Dumm?", description="*on* oder *off* als Option für diesen Befehl**!**", color=0x9d1092)
-    return(embed)
+        return('**Dumm?**\r\n*on* oder *off* als Option für diesen Befehl**!**')
 
-
-#Neu
-#async def play(message):
-#    channel = message.author.voice.channel
-#    global player
-#    try:
-#        player = await channel.connect()
-#    except:
-#        pass
-#
-#async def stop():
-#    player.stop()
+async def status_task():
+    while True:
+        global pornoseiten
+        await client.change_presence(activity=discord.Game(random.choice(pornoseiten)), status=discord.Status.online)
+        await asyncio.sleep(3)
+def token():
+    token = open('token.txt', 'r')
+    return token.read()
 
 client.run(token())
