039834c3d8849d3fb316a4f20a8f857b8c940274
Linus Feldmann <lfeldmann40@googlemail.com>
2021-10-05 19:14:05 +0200

    added youtube suport

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import discord
 import asyncio
 import random
 from functions import *
+from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from discord import TextChannel
+from youtube_dl import YoutubeDL
 
 #Umfragen
 client = discord.Client()
@@ -32,10 +37,14 @@ async def on_message(message):
             await message.channel.send(embed=randint(message))
         elif message.content.startswith('.joke'):
             await message.channel.send(embed=joke(message))
-#        elif message.content.startswith('.play'):
-#            await play(message)
-#        elif message.content.startswith('.stop'):
-#            await stop()
+        elif message.content.startswith('.play'):
+            await play(message)
+        elif message.content.startswith('.stop'):
+            await stop(message)
+        elif message.content.startswith('.pause'):
+            await pause(message)
+        elif message.content.startswith('.resume'):
+            await resume(message)
         else:
             await message.channel.send("**Dumm?**\r\nSiehe *.help* für mehr Informationen**!**")
     elif(autorepeat_status):
@@ -48,6 +57,59 @@ async def status_task():
     while True:
         await client.change_presence(activity=discord.Game(random.choice(pornoseiten)), status=discord.Status.online)
         await asyncio.sleep(3)
+
+async def play(message):
+    channel = message.author.voice.channel
+    voice = get(client.voice_clients, guild=message.guild)
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+    if not voice.is_playing():
+        url = message.content[len(".play "):]
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['url']
+        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice.is_playing()
+        await message.channel.send('Bot spielt: ' + url)
+
+# check if the bot is already playing
+    else:
+        await message.channel.send("Bot spielt bereits Musik")
+        return
+
+async def pause(message):
+    voice = get(client.voice_clients, guild=message.guild)
+
+    if voice.is_playing():
+        voice.pause()
+        await message.channel.send('Bot pausiert')
+
+async def resume(message):
+    voice = get(client.voice_clients, guild=message.guild)
+
+    if not voice.is_playing():
+        voice.resume()
+        await message.channel.send('Bot fährt fort')
+
+async def stop(message):
+    voice = get(client.voice_clients, guild=message.guild)
+    channel = message.author.voice.channel
+
+    try:
+        if voice.is_playing():
+            voice.stop()
+            await message.channel.send('Stoppt...')
+    except AttributeError:
+        channel.disconnect()
+        voice.disconnect()
+        channel.cleanup()
+        voice.cleanup()
 
 def autorepeat(message):
     global autorepeat_status
@@ -63,19 +125,19 @@ def autorepeat(message):
 
 def token():
     token = open('token.txt', 'r')
-    print("Token:" + token)
     return token.read()
 
 #Neu
-#async def play(message):
+#async def playown(message):
 #    channel = message.author.voice.channel
 #    global player
 #    try:
-#        player = await channel.connect()
+#        await connect(reconnect(True))
+#        play()
 #    except:
 #        pass
 #
 #async def stop():
-#    player.stop()
+#    player.disconnect()
 
 client.run(token())
