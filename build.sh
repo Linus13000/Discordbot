e0409e5ca63d4542380aacfa7fd9e400099e632f
Linus Feldmann <lfeldmann40@googlemail.com>
2022-01-12 20:48:35 +0100

    ready to publish

--- build.sh
+++ build.sh
@@ -3,3 +3,14 @@ python -m venv venv
 source venv/bin/activate
 venv/bin/python -m pip install --upgrade pip
 pip install -r requirements.txt
+echo
+echo
+echo
+read -p "Token: " token
+echo That worked!
+destfile=token.txt
+touch $destfile
+if [ -f "$destfile" ]
+then
+    echo "$token" > "$destfile"
+fi
